fc7ce0b5be7a2f539542cb5d0bf4bf6c428f9072
ArrowNate <George.Edmon@triosstudent.com>
2023-10-19 17:49:17 -0400

    Created A Weapon Script Folder inside the Scripts Folder and Created A Projectile Script

---
+++ GalacticGuardianII StellarCrusade/Assets/Scripts/Weapon Scripts/Projectile.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private AudioClip spawnSound;
+    [SerializeField] private AudioClip destroySound;
+    [SerializeField] private GameObject boomEffect;
+
+    public float minDamage = 10f;
+    public float maxDamage = 30f;
+
+    private float projectileDamage;
+
+    private void Start()
+    {
+        projectileDamage = (int)Random.Range(minDamage, maxDamage);
+
+        if (spawnSound)
+        {
+            AudioSource.PlayClipAtPoint(spawnSound, new Vector3(0f, 6f, 0f));
+        }
+    }
+
+    private void Update()
+    {
+        transform.Translate(0f, speed * Time.deltaTime, 0f);
+    }
+}
